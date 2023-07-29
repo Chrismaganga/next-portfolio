b3ee2b6f7211324ab9882b41476cbaa00159b68e
Chrismaganga <chrismaganga3@gmail.com>
2023-07-29 13:28:10 +0200

    update

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Calendar } from '@/components/ui/calendar'
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Calendar/>
+          </main>
+  )
+}
