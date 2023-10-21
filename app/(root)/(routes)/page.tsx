a37b99cb198c747b3c6aa17daf45ada5abcb78b5
Chrismaganga <chrismaganga3@gmail.com>
2023-10-21 13:50:25 +0200

    add

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,11 @@
-
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
  
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/"/>
+      return <Link href="/dashboard">Dashboard</Link>
     </div>
   )
 }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+ 
+export default function Page() {
+  return (
+    <div className='items-center'>dashboard</div>
+  )
+}
